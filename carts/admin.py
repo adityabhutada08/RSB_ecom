aee24ca3092394be9258c47111be9ba0d01b5cc1
adityabhutada08 <080202adi.bhutada@gmail.com>
2024-09-20 18:29:34 +0530

    Too many changes have been done, modified settings, accounts view, carts app and changes in the templates

--- carts/admin.py
+++ carts/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from.models import Cart, CartItem
+from .models import Cart, CartItem
 # Register your models here.
 
-admin.site.register(Cart)
-admin.site.register(CartItem)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user','cart_id', 'date_added')
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('user','product', 'cart', 'quantity', 'is_active', 'date_added')
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem, CartItemAdmin)
--- carts/models.py
+++ carts/models.py
@@ -1,25 +1,33 @@
 from django.db import models
-from store.models import Product
+from store.models import Product, Variation
+from accounts.models import Account
+from django.utils import timezone
+
 
 # Create your models here.
 
 class Cart(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.cart_id
-    
+
 
 class CartItem(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(Variation, blank=True)
+    cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
+    date_added = models.DateTimeField(default=timezone.now, blank=True)
+    
+
 
     def sub_total(self):
         return self.product.price * self.quantity
-        cart_items = CartItem.objects.filter(cart__cart_id=request.session.session_key)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.product
